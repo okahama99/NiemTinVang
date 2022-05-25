2cdefd60d09813b5e07db87c13feb97923297e2b
Okahama <48874863+Okahama@users.noreply.github.com>
2022-05-25 14:55:17 +0700

    Initial commit

---
+++ src/main/java/com/example/ntv/NtvApplication.java
@@ -0,0 +1,13 @@
+package com.example.ntv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NtvApplication.class, args);
+    }
+
+}
