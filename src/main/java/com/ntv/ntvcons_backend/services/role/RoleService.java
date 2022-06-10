7caade54b63dcbe7222c3bc7df726dab07b5793a
ThanhNMSE130286 <thanhnmse130286@fpt.edu.vn>
2022-06-10 17:46:36 +0700

    CRUD role

--- src/main/java/com/ntv/ntvcons_backend/services/role/RoleService.java
+++ src/main/java/com/ntv/ntvcons_backend/services/role/RoleService.java
@@ -2,33 +2,32 @@ package com.ntv.ntvcons_backend.services.role;
 
 import com.ntv.ntvcons_backend.dtos.role.RoleDTO;
 import com.ntv.ntvcons_backend.entities.Role;
-import com.ntv.ntvcons_backend.entities.RoleModels.ShowRoleModel;
 
 import java.util.Collection;
 import java.util.List;
 
 public interface RoleService {
     /* CREATE */
-    Role createRole(Role newRole);
-    RoleDTO createRoleByDTO(RoleDTO newRoleDTO);
+    Role createRole(Role newRole) throws Exception;
+    RoleDTO createRoleByDTO(RoleDTO newRoleDTO) throws Exception;
 
     /* READ */
-    List<Role> getAll(int pageNo, int pageSize, String sortBy, boolean sortType);
-    List<RoleDTO> getAllDTO(int pageNo, int pageSize, String sortBy, boolean sortType);
+    List<Role> getAll(int pageNo, int pageSize, String sortBy, boolean sortType) throws Exception;
+    List<RoleDTO> getAllDTO(int pageNo, int pageSize, String sortBy, boolean sortType) throws Exception;
 
-    List<Role> getAllByRoleNameLike(String roleName);
-    List<RoleDTO> getAllDTOByRoleNameLike(String roleName);
+    List<Role> getAllByRoleNameLike(String roleName) throws Exception;
+    List<RoleDTO> getAllDTOByRoleNameLike(String roleName) throws Exception;
 
-    Role getById(long roleId);
-    RoleDTO getDTOById(long roleId);
+    Role getById(long roleId) throws Exception;
+    RoleDTO getDTOById(long roleId) throws Exception;
 
-    Role getAllByIdIn(Collection<Long> roleIdCollection);
-    RoleDTO getAllDTOByIdIn(Collection<Long> roleIdCollection);
+    List<Role> getAllByIdIn(Collection<Long> roleIdCollection) throws Exception;
+    List<RoleDTO> getAllDTOByIdIn(Collection<Long> roleIdCollection) throws Exception;
 
     /* UPDATE */
-    Role updateRole(Role updatedRole);
-    RoleDTO updateRoleByDTO(RoleDTO updatedRoleDTO);
+    Role updateRole(Role updatedRole) throws Exception;
+    RoleDTO updateRoleByDTO(RoleDTO updatedRoleDTO) throws Exception;
 
     /* DELETE */
-    boolean deleteRole(long roleID);
+    boolean deleteRole(long roleId) throws Exception;
 }
--- src/main/java/com/ntv/ntvcons_backend/services/role/RoleServiceImpl.java
+++ src/main/java/com/ntv/ntvcons_backend/services/role/RoleServiceImpl.java
@@ -1,10 +1,9 @@
 package com.ntv.ntvcons_backend.services.role;
 
-import com.google.common.base.Converter;
-import com.ntv.ntvcons_backend.dtos.role.RoleDTO;
 import com.ntv.ntvcons_backend.entities.Role;
-import com.ntv.ntvcons_backend.entities.RoleModels.ShowRoleModel;
+import com.ntv.ntvcons_backend.dtos.role.RoleDTO;
 import com.ntv.ntvcons_backend.repositories.RoleRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -12,122 +11,165 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
-public class RoleServiceImpl implements RoleService{
+public class RoleServiceImpl implements RoleService {
     @Autowired
-    RoleRepository roleRepository;
+    private RoleRepository roleRepository;
+    @Autowired
+    private ModelMapper modelMapper;
 
     /* CREATE */
     @Override
-    public Role createRole(Role newRole) {
-
+    public Role createRole(Role newRole) throws Exception {
         return roleRepository.saveAndFlush(newRole);
     }
     @Override
-    public RoleDTO createRoleByDTO(RoleDTO newRoleDTO) {
-//        Role role = new Role();
-////        role.setName(inputtedRole);
-////        role.setStatus(Status.ACTIVE);
-////        roleRepository.saveAndFlush(role);
-//        return role;
-        return null;
+    public RoleDTO createRoleByDTO(RoleDTO newRoleDTO) throws Exception {
+        Role newRole = modelMapper.map(newRoleDTO, Role.class);
+
+        newRole = createRole(newRole);
+
+        return modelMapper.map(newRole, RoleDTO.class);
     }
 
     /* READ */
     @Override
-    public List<Role> getAll(int pageNo, int pageSize, String sortBy, boolean sortType) {
-//        Pageable paging;
-//        if(sortType) {
-//            paging = PageRequest.of(pageNo, pageSize, Sort.by(sortBy).ascending());
-//        }else{
-//            paging = PageRequest.of(pageNo, pageSize, Sort.by(sortBy).descending());
-//        }
-//        Page<Role> pagingResult = roleRepository.findAllByIsDeletedIsFalse(paging);
-//        if(pagingResult.hasContent()){
-//            double totalPage = Math.ceil((double)pagingResult.getTotalElements() / pageSize);
-//            Page<ShowRoleModel> modelResult = pagingResult.map(new Converter<Role, ShowRoleModel>() {
-//                @Override
-//                protected ShowRoleModel doForward(Role role) {
-//                    ShowRoleModel model = new ShowRoleModel();
-//                    //TODO
-//                    return model;
-//                }
-//
-//                @Override
-//                protected Role doBackward(ShowRoleModel showRoleModel) {
-//                    return null;
-//                }
-//            });
-//            return modelResult.getContent();
-//        }else{
-//            return new ArrayList<ShowRoleModel>();
-//        }
-        return null;
+    public List<Role> getAll(int pageNo, int pageSize, String sortBy, boolean sortType) throws Exception {
+        Pageable paging;
+        if (sortType) {
+            paging = PageRequest.of(pageNo, pageSize, Sort.by(sortBy).ascending());
+        } else {
+            paging = PageRequest.of(pageNo, pageSize, Sort.by(sortBy).descending());
+        }
+
+        Page<Role> rolePage = roleRepository.findAllByIsDeletedIsFalse(paging);
+
+        if (!rolePage.hasContent()) {
+            return null;
+        }
+
+        return rolePage.getContent();
     }
     @Override
-    public List<RoleDTO> getAllDTO(int pageNo, int pageSize, String sortBy, boolean sortType) {
-        return null;
+    public List<RoleDTO> getAllDTO(int pageNo, int pageSize, String sortBy, boolean sortType) throws Exception {
+        List<Role> roleList = getAll(pageNo, pageSize, sortBy, sortType);
+
+        if (roleList != null && !roleList.isEmpty()) {
+//            double totalPage = Math.ceil((double) roleList.size() / pageSize);
+
+            return roleList.stream().map(role -> modelMapper.map(role, RoleDTO.class))
+                    .collect(Collectors.toList());
+
+        } else {
+            return null;
+        }
     }
 
     @Override
-    public List<Role> getAllByRoleNameLike(String roleName) {
-        return null;
+    public List<Role> getAllByRoleNameLike(String roleName) throws Exception {
+        List<Role> roleList = roleRepository.findAllByRoleNameLikeAndIsDeletedIsFalse(roleName);
+
+        if (roleList.isEmpty()) {
+            return null;
+        }
+
+        return roleList;
     }
     @Override
-    public List<RoleDTO> getAllDTOByRoleNameLike(String roleName) {
-        return null;
+    public List<RoleDTO> getAllDTOByRoleNameLike(String roleName) throws Exception {
+        List<Role> roleList = getAllByRoleNameLike(roleName);
+
+        if (roleList == null) {
+            return null;
+        }
+
+        return roleList.stream().map(role -> modelMapper.map(role, RoleDTO.class))
+                .collect(Collectors.toList());
     }
 
     @Override
-    public Role getById(long roleId) {
-        return null;
+    public Role getById(long roleId) throws Exception {
+        Optional<Role> role = roleRepository.findByRoleIdAndIsDeletedIsFalse(roleId);
+
+        return role.orElse(null);
     }
     @Override
-    public RoleDTO getDTOById(long roleId) {
-        return null;
+    public RoleDTO getDTOById(long roleId) throws Exception {
+        Role role = getById(roleId);
+
+        if (role == null) {
+            return null;
+        }
+
+        return modelMapper.map(role, RoleDTO.class);
     }
 
     @Override
-    public Role getAllByIdIn(Collection<Long> roleIdCollection) {
-        return null;
+    public List<Role> getAllByIdIn(Collection<Long> roleIdCollection) throws Exception {
+        List<Role> roleList = roleRepository.findAllByRoleIdInAndIsDeletedIsFalse(roleIdCollection);
+
+        if (roleList.isEmpty()) {
+            return null;
+        }
+
+        return roleList;
     }
     @Override
-    public RoleDTO getAllDTOByIdIn(Collection<Long> roleIdCollection) {
-        return null;
+    public List<RoleDTO> getAllDTOByIdIn(Collection<Long> roleIdCollection) throws Exception {
+        List<Role> roleList = getAllByIdIn(roleIdCollection);
+
+        if (roleList == null) {
+            return null;
+        }
+
+        return roleList.stream().map(role -> modelMapper.map(role, RoleDTO.class))
+                .collect(Collectors.toList());
     }
 
     /* UPDATE */
     @Override
-    public Role updateRole(Role updatedRole) {
-//        Role result = roleRepository.findById(roleID).get();
-//        result.setName(role.getName());
-//        result.setStatus(role.getStatus());
-//        roleRepository.saveAndFlush(result);
-//        if(result != null)
-//        {
-//            return true;
-//        }
-        return null;
+    public Role updateRole(Role updatedRole) throws Exception {
+        Optional<Role> role = roleRepository.findByRoleIdAndIsDeletedIsFalse(updatedRole.getRoleId());
+
+        if (!role.isPresent()) {
+            return null;
+            /* Not found by Id, return null */
+        }
+
+        return roleRepository.save(updatedRole);
     }
     @Override
-    public RoleDTO updateRoleByDTO(RoleDTO updatedRoleDTO) {
-        return null;
+    public RoleDTO updateRoleByDTO(RoleDTO updatedRoleDTO) throws Exception {
+        Role updatedRole = modelMapper.map(updatedRoleDTO, Role.class);
+
+        updatedRole = updateRole(updatedRole);
+
+        if (updatedRole == null) {
+            return null;
+        }
+
+        return modelMapper.map(updatedRole, RoleDTO.class);
     }
 
     /* DELETE */
     @Override
-    public boolean deleteRole(long roleID) {
-        Role role = roleRepository.findById(roleID).get();
-        if(role != null){
-//            role.setStatus(Status.INACTIVE);
-//            roleRepository.save(role);
-            return true;
-        }else{
+    public boolean deleteRole(long roleId) throws Exception {
+        Optional<Role> role = roleRepository.findByRoleIdAndIsDeletedIsFalse(roleId);
+
+        if (!role.isPresent()) {
             return false;
+            /* Not found with Id */
         }
+
+        role.get().setIsDeleted(true);
+
+        roleRepository.saveAndFlush(role.get());
+
+        return true;
     }
 }
