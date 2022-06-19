9ad0c451475a2e7e39e39da0d0de7cc77c3e4d75
Okahama <48874863+Okahama@users.noreply.github.com>
2022-06-19 10:48:12 +0700

    Done RequestDetail

--- src/main/java/com/ntv/ntvcons_backend/entities/RequestDetailModels/ShowRequestDetailModel.java
+++ src/main/java/com/ntv/ntvcons_backend/entities/RequestDetailModels/ShowRequestDetailModel.java
@@ -3,7 +3,16 @@ package com.ntv.ntvcons_backend.entities.RequestDetailModels;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
+
 @Getter
 @Setter
 public class ShowRequestDetailModel {
+    private Long requestDetailId, requestId;
+    private String itemDesc, itemUnit;
+    private Double itemAmount, itemPrice;
+
+    private Long createdBy, updatedBy;
+    private Date createdAt, updatedAt;
+    private Double totalPage;
 }
