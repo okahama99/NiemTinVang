7a9d076a4e5923a4d74210c87e653835e0fbf8b4
ThanhNMSE130286 <thanhnmse130286@fpt.edu.vn>
2022-07-10 01:05:34 +0700

    project manager

--- src/main/java/com/ntv/ntvcons_backend/dtos/projectManager/ProjectManagerCreateDTO.java
+++ src/main/java/com/ntv/ntvcons_backend/dtos/projectManager/ProjectManagerCreateDTO.java
@@ -10,12 +10,13 @@ import lombok.NoArgsConstructor;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProjectManagerCreateDTO extends BaseCreateDTO {
-    @Positive
+    @PositiveOrZero
     @NotNull(message = "projectId REQUIRED for create")
     private Long projectId;
 
--- src/main/java/com/ntv/ntvcons_backend/services/projectManager/ProjectManagerService.java
+++ src/main/java/com/ntv/ntvcons_backend/services/projectManager/ProjectManagerService.java
@@ -1,8 +1,11 @@
 package com.ntv.ntvcons_backend.services.projectManager;
 
+import com.ntv.ntvcons_backend.dtos.projectManager.ProjectManagerCreateDTO;
 import com.ntv.ntvcons_backend.dtos.projectManager.ProjectManagerReadDTO;
 import com.ntv.ntvcons_backend.dtos.projectManager.ProjectManagerUpdateDTO;
 import com.ntv.ntvcons_backend.entities.ProjectManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Collection;
 import java.util.List;
@@ -10,25 +13,47 @@ import java.util.List;
 public interface ProjectManagerService {
     /* CREATE */
     ProjectManager createProjectManager(ProjectManager newProjectManager) throws Exception;
+    ProjectManagerReadDTO createProjectManagerByDTO(ProjectManagerCreateDTO newProjectManagerDTO) throws Exception;
 
     List<ProjectManager> createBulkProjectManager(List<ProjectManager> newProjectManagerList) throws Exception;
+    List<ProjectManagerReadDTO> createBulkProjectManagerByDTO(List<ProjectManagerCreateDTO> newProjectManagerDTOList) throws Exception;
 
     /* READ */
-    List<ProjectManager> getAll(int pageNo, int pageSize, String sortBy, boolean sortType) throws Exception;
+    Page<ProjectManager> getPageAll(Pageable paging) throws Exception;
+    List<ProjectManagerReadDTO> getAllDTOInPaging(Pageable paging) throws Exception;
 
     ProjectManager getById(long projectManagerId) throws Exception;
+    ProjectManagerReadDTO getDTOById(long projectManagerId) throws Exception;
 
     List<ProjectManager> getAllByIdIn(Collection<Long> projectManagerIdCollection) throws Exception;
+    List<ProjectManagerReadDTO> getAllDTOByIdIn(Collection<Long> projectManagerIdCollection) throws Exception;
 
     List<ProjectManager> getAllByManagerId(long managerId) throws Exception;
+    List<ProjectManagerReadDTO> getAllDTOByManagerId(long managerId) throws Exception;
+    Page<ProjectManager> getPageAllByManagerId(Pageable paging, long managerId) throws Exception;
+    List<ProjectManagerReadDTO> getAllDTOInPagingByManagerId(Pageable paging, long managerId) throws Exception;
+
+    List<ProjectManager> getAllByManagerIdIn(Collection<Long> managerIdCollection) throws Exception;
+    List<ProjectManagerReadDTO> getAllDTOByManagerIdIn(Collection<Long> managerIdCollection) throws Exception;
+    Page<ProjectManager> getPageAllByManagerIdIn(Pageable paging, Collection<Long> managerIdCollection) throws Exception;
+    List<ProjectManagerReadDTO> getAllDTOInPagingByManagerIdIn(Pageable paging, Collection<Long> managerIdCollection) throws Exception;
 
     List<ProjectManager> getAllByProjectId(long projectId) throws Exception;
+    List<ProjectManagerReadDTO> getAllDTOByProjectId(long projectId) throws Exception;
+    Page<ProjectManager> getPageAllByProjectId(Pageable paging, long projectId) throws Exception;
+    List<ProjectManagerReadDTO> getAllDTOInPagingByProjectId(Pageable paging, long projectId) throws Exception;
+
+    List<ProjectManager> getAllByProjectIdIn(Collection<Long> projectIdCollection) throws Exception;
+    List<ProjectManagerReadDTO> getAllDTOByProjectIdIn(Collection<Long> projectIdCollection) throws Exception;
+    Page<ProjectManager> getPageAllByProjectIdIn(Pageable paging, Collection<Long> projectIdCollection) throws Exception;
+    List<ProjectManagerReadDTO> getAllDTOInPagingByProjectIdIn(Pageable paging, Collection<Long> projectIdCollection) throws Exception;
 
     /* UPDATE */
     ProjectManager updateProjectManager(ProjectManager updatedProjectManager) throws Exception;
     ProjectManagerReadDTO updateProjectManagerByDTO(ProjectManagerUpdateDTO updatedProjectManagerDTO) throws Exception;
 
     List<ProjectManager> updateBulkProjectManager(List<ProjectManager> updatedProjectManagerList) throws Exception;
+    List<ProjectManagerReadDTO> updateBulkProjectManagerByDTO(List<ProjectManagerUpdateDTO> updatedProjectManagerDTOList) throws Exception;
 
     /* DELETE */
     boolean deleteProjectManager(long projectManagerId) throws Exception;
