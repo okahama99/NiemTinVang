a3eb3f77e75d0862a8343d3ad67bdeda3cc02e24
ThanhNMSE130286 <thanhnmse130286@fpt.edu.vn>
2022-08-15 03:00:59 +0700

    replace old chat table (db) & entity remove notification table (db) & entity

---
+++ src/main/java/com/ntv/ntvcons_backend/entities/Conversation.java
@@ -0,0 +1,33 @@
+package com.ntv.ntvcons_backend.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "conversation")
+public class Conversation extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "conversationId", nullable = false)
+    private Long conversationId;
+
+    @Column(name = "user1Id", nullable = false)
+    private Long user1Id;
+
+    @Column(name = "user2Id")
+    private Long user2Id;
+
+    @Column(name = "ipAddress", length = 61)
+    private String ipAddress;
+
+    @Column(name = "clientName")
+    private String clientName;
+}
---
+++ src/main/java/com/ntv/ntvcons_backend/entities/Message.java
@@ -0,0 +1,41 @@
+package com.ntv.ntvcons_backend.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "message")
+public class Message extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "messageId", nullable = false)
+    private Long messageId;
+
+    @Column(name = "conversationId", nullable = false)
+    private Long conversationId;
+
+    @Column(name = "senderId")
+    private Long senderId;
+
+    /* IPv6 max 61 chars */
+    @Column(name = "senderIp", length = 61)
+    private String senderIp;
+
+    @Column(name = "sendTime", nullable = false)
+    private LocalDateTime sendTime;
+
+    @Column(name = "message")
+    private String message;
+
+    @Column(name = "seen", nullable = false)
+    private Boolean seen = false;
+}
