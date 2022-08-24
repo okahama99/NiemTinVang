c4c2e03b2a7a28b2382ee01abea5229fb396f181
ThanhNMSE130286 <thanhnmse130286@fpt.edu.vn>
2022-08-24 19:19:49 +0700

    minor change input name

--- src/main/java/com/ntv/ntvcons_backend/controllers/MessageController.java
+++ src/main/java/com/ntv/ntvcons_backend/controllers/MessageController.java
@@ -8,8 +8,9 @@ import com.ntv.ntvcons_backend.entities.MessageModels.ShowMessageModel;
 import com.ntv.ntvcons_backend.services.message.MessageService;
 import com.ntv.ntvcons_backend.services.misc.FileCombineService;
 import com.ntv.ntvcons_backend.utils.MiscUtil;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Description;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -33,33 +34,36 @@ public class MessageController {
     private FileCombineService fileCombineService;
 
     @GetMapping(value = "/v1/getByConversationId", produces = "application/json;charset=UTF-8")
-    public ResponseEntity<Object> getByConversationId(@RequestParam String userIdOrclientIp,
-                                                      @RequestParam SearchType.MESSAGE searchType,
-                                                      @RequestParam Long conversationId,
-                                                      @RequestParam int pageNo,
-                                                      @RequestParam int pageSize,
-                                                      @RequestParam("ghi la messageId") String sortBy,
-                                                      @RequestParam boolean sortTypeAsc) {
+    public ResponseEntity<Object> getByConversationId(
+            @RequestParam String userIdOrClientIp,
+            @RequestParam SearchType.MESSAGE searchType,
+            @RequestParam Long conversationId,
+            @RequestParam int pageNo,
+            @RequestParam int pageSize,
+            @Parameter(example = "messageId") @RequestParam String sortBy,
+            @RequestParam boolean sortTypeAsc) {
         try {
             Pageable paging = miscUtil.makePaging(pageNo, pageSize, sortBy, sortTypeAsc);
             List<ShowMessageModel> list;
 
             switch (searchType) {
                 case BY_CONVERSATION_ID_AUTHENTICATED:
-                    list = messageService.getByConversationIdAuthenticated(Long.parseLong(userIdOrclientIp), conversationId, paging);
+                    list = messageService
+                            .getByConversationIdAuthenticated(
+                                    Long.parseLong(userIdOrClientIp), conversationId, paging);
 
                     if (list == null) {
                         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                                .body("No Message found with Id: '" + userIdOrclientIp + "'. ");
+                                .body("No Message found with Id: '" + userIdOrClientIp + "'. ");
                     }
                     break;
 
                 case BY_CONVERSATION_ID_UNAUTHENTICATED:
-                    list = messageService.getByConversationIdUnauthenticated(userIdOrclientIp, conversationId, paging);
+                    list = messageService.getByConversationIdUnauthenticated(userIdOrClientIp, conversationId, paging);
 
                     if (list == null) {
                         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                                .body("No Message found with IPAddress: '" + userIdOrclientIp + "'. ");
+                                .body("No Message found with IPAddress: '" + userIdOrClientIp + "'. ");
                     }
                     break;
 
@@ -83,11 +87,11 @@ public class MessageController {
 
             switch (searchType) {
                 case BY_CONVERSATION_ID_AUTHENTICATED:
-                    errorMsg += "Id: '" + userIdOrclientIp + "'. ";
+                    errorMsg += "Id: '" + userIdOrClientIp + "'. ";
                     break;
 
                 case BY_CONVERSATION_ID_UNAUTHENTICATED:
-                    errorMsg += "IPAddress: '" + userIdOrclientIp + "'. ";
+                    errorMsg += "IPAddress: '" + userIdOrClientIp + "'. ";
                     break;
             }
 
@@ -97,10 +101,11 @@ public class MessageController {
 
     @PreAuthorize("hasAnyAuthority('24','54','14','34','44','4')")
     @PostMapping(value = "/v1/sendMessageAuthenticated", produces = "application/json;charset=UTF-8", consumes = "multipart/form-data")
-    public ResponseEntity<Object> sendMessageAuthenticated(@RequestParam Long userId,
-                                                           @RequestParam Long conversationId,
-                                                           @RequestParam String message,
-                                                           @RequestPart(required = false) List<MultipartFile> file) throws Exception {
+    public ResponseEntity<Object> sendMessageAuthenticated(
+            @RequestParam Long userId,
+            @RequestParam Long conversationId,
+            @RequestParam String message,
+            @RequestPart(required = false) List<MultipartFile> file) throws Exception {
 
         Long messageId = messageService.sendMessageAuthenticated(userId, conversationId, message);
 
@@ -109,16 +114,18 @@ public class MessageController {
                 fileCombineService.saveAllFileInDBAndFirebase(
                         file, FileType.MESSAGE_FILE, messageId, EntityType.MESSAGE_ENTITY, userId);
             }
+
             return ResponseEntity.ok().body("Gửi thành công.");
         }
         return ResponseEntity.badRequest().body("Gửi thất bại.");
     }
 
     @PostMapping(value = "/v1/sendMessageUnauthenticated", produces = "application/json;charset=UTF-8", consumes = "multipart/form-data")
-    public ResponseEntity<Object> sendMessageUnauthenticated(@RequestParam String ipAddress,
-                                                             @RequestParam Long conversationId,
-                                                             @RequestParam String message,
-                                                             @RequestPart(required = false) List<MultipartFile> file) throws Exception {
+    public ResponseEntity<Object> sendMessageUnauthenticated(
+            @RequestParam String ipAddress,
+            @RequestParam Long conversationId,
+            @RequestParam String message,
+            @RequestPart(required = false) List<MultipartFile> file) throws Exception {
 
         Long messageId = messageService.sendMessageUnauthenticated(ipAddress, conversationId, message);
 
@@ -127,6 +134,7 @@ public class MessageController {
                 fileCombineService.saveAllFileInDBAndFirebase(
                         file, FileType.MESSAGE_FILE, messageId, EntityType.MESSAGE_ENTITY, messageId);
             }
+
             return ResponseEntity.ok().body("Gửi thành công.");
         }
         return ResponseEntity.badRequest().body("Gửi thất bại.");
